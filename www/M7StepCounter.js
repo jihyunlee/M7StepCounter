3880819b38c3631ae3e04d162f45a34dc7c3c888
jihyun <jihyunlee@users.noreply.github.com>
2014-04-17 20:18:09 -0400

    Update M7StepCounter.js

--- www/M7StepCounter.js
+++ www/M7StepCounter.js
@@ -25,19 +25,19 @@ var M7StepCounter = function() {
 	this.serviceName = "M7StepCounter";
 };
 
-M7StepCounter.prototype.getForKey = function(successCallback, failureCallback, key, servicename)
+M7StepCounter.prototype.isAvailable = function(successCallback, failureCallback)
 {
-	exec(successCallback, failureCallback, this.serviceName, "getForKey", [key, servicename]);
+	exec(successCallback, failureCallback, this.serviceName, "isAvailable", []);
 }
 
-M7StepCounter.prototype.setForKey = function(successCallback, failureCallback, key, servicename, value)
+M7StepCounter.prototype.start = function(successCallback, failureCallback)
 {
-	exec(successCallback, failureCallback, this.serviceName, "setForKey", [key, servicename, value]);
+	exec(successCallback, failureCallback, this.serviceName, "start", []);
 }
 
-M7StepCounter.prototype.removeForKey = function(successCallback, failureCallback, key, servicename)
+M7StepCounter.prototype.stop = function(successCallback, failureCallback)
 {
-	exec(successCallback, failureCallback, this.serviceName, "removeForKey", [key, servicename]);
+	exec(successCallback, failureCallback, this.serviceName, "stop", []);
 }
 
 module.exports = M7StepCounter;
