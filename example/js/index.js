8907583ff9cc0c4b26ce0350e89d768736cb73e1
j <treeofez@gmail.com>
2014-04-17 22:17:15 -0400

    add example

---
+++ example/js/index.js
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        if(window.cordova.logger) {
+            window.cordova.logger.__onDeviceReady();
+        }
+        
+        app.isAvailable();
+        console.log('ho');
+    },
+    isAvailable: function() {
+        console.log('isAvailable');
+        console.log('M7StepCounter',M7StepCounter);
+        M7StepCounter.isAvailable(app.onAvailable, app.generateFailureFunction("isAvailable Failed"));
+    },
+    onAvailable: function(res) {
+        console.log('onAvailable', res);
+    },
+    generateFailureFunction: function(message) {
+        console.log('generateFailure', message);
+        var func = function(reason) {
+            var details = "";
+            if (reason) {
+                details += ": " + JSON.stringify(reason);
+            }
+            app.setStatus(message + details);
+        };
+        return func;
+    }
+};
--- www/M7StepCounter.js
+++ www/M7StepCounter.js
@@ -27,21 +27,25 @@ var M7StepCounter = function() {
 
 M7StepCounter.prototype.isAvailable = function(successCallback, failureCallback)
 {
+	console.log('M7StepCounter.prototype.isAvailable');
 	exec(successCallback, failureCallback, this.serviceName, "isAvailable", []);
 }
 
 M7StepCounter.prototype.start = function(successCallback, failureCallback)
 {
+	console.log('M7StepCounter.prototype.start');
 	exec(successCallback, failureCallback, this.serviceName, "start", []);
 }
 
 M7StepCounter.prototype.stop = function(successCallback, failureCallback)
 {
+	console.log('M7StepCounter.prototype.stop');
 	exec(successCallback, failureCallback, this.serviceName, "stop", []);
 }
 
 M7StepCounter.prototype.getSteps = function(successCallback, failureCallback)
 {
+	console.log('M7StepCounter.prototype.getSteps');
 	exec(successCallback, failureCallback, this.serviceName, "getSteps", []);
 }
 
